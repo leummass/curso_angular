24c3aaffad34cdeabb89159d06c07fb24fe16893
Samuel <jesuval420@gmail.com>
2023-07-05 23:24:35 -0700

    Día 2

--- curso_angular/primera-aplicacion/src/app/app.component.ts
+++ curso_angular/primera-aplicacion/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primera-aplicacion';
+  title = 'Aplicación para registro de nuevos usuarios';
 }

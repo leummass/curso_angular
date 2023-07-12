0d54925baa03fc20996d3c02d0e644237cd2ae27
Samuel <jesuval420@gmail.com>
2023-07-12 00:05:54 -0700

    Día 5

--- curso_angular/primera-aplicacion/src/app/usuarios/usuarios.component.ts
+++ curso_angular/primera-aplicacion/src/app/usuarios/usuarios.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-usuarios',
-    templateUrl: './usuarios.component.html'
+    templateUrl: './usuarios.component.html',
+    styleUrls: ['./usuarios.component.css'],
 })
 
 export class UsuariosComponent{}

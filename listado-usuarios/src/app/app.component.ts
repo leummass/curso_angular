3e5b90aadf8a6841b53f35b021a36c2da522cddd
Samuel <jesuval420@gmail.com>
2023-07-23 23:51:14 -0700

    Día 10

--- listado-usuarios/src/app/app.component.ts
+++ listado-usuarios/src/app/app.component.ts
@@ -13,4 +13,14 @@ export class AppComponent {
     new Usuario('Laura', 'Rojo'),
     new Usuario('Pedro','Picapiedra')
   ];
+  nombreInput:string='';
+  apellidoInput:string='';
+  agregarUsuario(){
+    let usuarion = new Usuario(this.nombreInput,this.apellidoInput);
+    this.usuarios.push(usuarion);
+  }
+  eliminarUsuario(){
+    this.usuarios.pop();
+  }
+
 }
